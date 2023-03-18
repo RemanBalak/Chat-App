decd0e70c0da83f78666a88e8a6d869b44383271
unknown <reman1999@outlook.com>
2023-03-18 13:40:53 +0100

    Implemented UI for Chat screen

--- components/Chat.js
+++ components/Chat.js
@@ -1,25 +1,87 @@
-import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  Platform,
+  KeyboardAvoidingView,
+} from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
-export default class Chat extends Component {
+export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
   componentDidMount() {
-    const name = this.props.route.params.name;
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        // system messages to display the last time a use was active on the app
+        {
+          _id: 2,
+          text: 'You have entered the Chat',
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
+    let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
   }
-  render() {
-    const backgroundColor = this.props.route.params.color;
+
+  //to send messages
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  // to change the speech bubble color
+  renderBubble(props) {
     return (
-      <View style={[styles.chatContainer, { backgroundColor }]}>
-        <TouchableOpacity
-          onPress={() => this.props.navigation.navigate('Start')}
-        >
-          <Text style={{ color: '#FFF', fontSize: 24 }}>Go to Start</Text>
-        </TouchableOpacity>
+      <Bubble
+        // ...props to inherit props
+        {...props}
+        wrapperStyle={{
+          // to target the right speech bubbles (the sender)
+          right: {
+            backgroundColor: '#4B0082',
+          },
+        }}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+
+        {/* fixes the keyboard entering the input box */}
+        {Platform.OS === 'android' ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : null}
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  chatContainer: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-});
